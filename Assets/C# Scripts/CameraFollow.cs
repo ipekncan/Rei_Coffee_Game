1d282177f0cef19b87c5e90333136dc2470d563e
İpek Nezihe Can <ipekn.can@gmail.com>
2026-05-05 18:33:11 +0300

    Player Controllers updated and isometric main camera added.

---
+++ Assets/C# Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // The target the camera will follow
+    public Vector3 offset = new Vector3(10f, 10f, -10f);
+    public float smoothSpeed = 5f;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    //Not:kamera takibini Late Update'e koymamýzýn sebebi, karakterin hareketini Update'de yaparken kameranýn hareketini Late Update'de yaparak, karakterin hareketi tamamlandýktan sonra kameranýn pozisyonunu güncellemesini saðlamaktýr. Bu sayede kamera, karakterin hareketine daha düzgün ve akýcý bir þekilde tepki verebilir.
+    void LateUpdate()
+    {
+        if (target == null) { return; }
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        transform.LookAt(target.position + Vector3.up * 1.5f);
+    }
+}
---
+++ Assets/C# Scripts/PlayerController.cs
@@ -0,0 +1,176 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement Settings")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f; 
+    public float rotationSpeed = 700f;
+    
+    [Header("Player States")]
+    public int playerHealth = 100;
+    public int playerMoney = 0;
+    public bool isDead = false;
+
+    //state variables
+    private bool isSprinting = false;
+    private bool isCarrying = false;
+    private int activeSlot = 0;//we will use this to track which item slot is active for inventory management
+
+
+    private Animator animator;
+    private CharacterController controller;
+    private Vector3 movement;
+    private float verticalVelocity;
+    
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    //-----INPUT SYSTEM ACTIONS -----//
+    public void OnMove(InputValue value)
+    {
+        if (isDead == true) { return; }
+        Vector2 inputVector = value.Get<Vector2>();
+        movement = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
+   
+    public void OnSprint(InputValue value)
+    {
+        isSprinting = value.isPressed;
+    }
+
+    public void OnAttack(InputValue value)
+    {
+        if (isDead == true) { return; }
+        if (value.isPressed && !isCarrying)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    // This method checks for nearby objects within a certain radius and returns the first one that is tagged as "Machine" or "Item".So we can use 'E' for many purposes
+    private GameObject GetNearbyObject()
+    {
+        float detectionRadius = 2.0f;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        foreach (var hitCollider in hitColliders)
+        {
+
+            if (hitCollider.CompareTag("Machine") ||
+                hitCollider.CompareTag("Item") ||
+                hitCollider.CompareTag("FieldArea"))
+            {
+                return hitCollider.gameObject;
+            }
+        }
+        return null;
+    }
+    public void OnInteract(InputValue value)
+    {
+        if (value.isPressed && !isDead)
+        {
+            
+            GameObject target = GetNearbyObject();
+
+            if (target != null)
+            {
+                
+                if (target.CompareTag("Machine"))
+                {
+                    //machine interaction minigame trigger
+                    animator.SetTrigger("isInteracting"); 
+                }
+                else if (target.CompareTag("Item"))
+                {
+
+                    animator.SetTrigger("UseItem"); 
+                }
+
+                else if (target.CompareTag("FieldArea"))
+                {
+                    animator.SetTrigger("isInteracting");
+                    ToggleCarrying();
+                }
+            }
+        }
+    }
+
+    private void ToggleCarrying()
+    {
+        isCarrying = !isCarrying;
+        animator.SetBool("IsCarrying", isCarrying);
+        Debug.Log("Carrying state toggled: " + isCarrying);
+
+    }
+    void Update()
+    {
+        if(isDead== true) { return; }
+        ApplyGravity();
+        MovePlayer();
+        HandleRotation();
+    }
+
+    void MovePlayer()
+    {
+        float currentSpeed = isSprinting ? runSpeed : walkSpeed;
+        Vector3 velocity = movement * currentSpeed;
+        velocity.y = verticalVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        // Blend Tree Parameter (0: Idle, 1: Walk, 2: Run)
+        float animSpeed = movement.magnitude * (isSprinting ? 2.0f : 1.0f);
+        animator.SetFloat("Speed", animSpeed);
+    }
+
+    void ApplyGravity()
+    {
+        if (controller.isGrounded) verticalVelocity = -0.5f;
+        else verticalVelocity += Physics.gravity.y * Time.deltaTime;
+    }
+
+    void HandleRotation()
+    {
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+   
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead==true) { return; }
+        playerHealth -= damage;
+        animator.SetTrigger("TakeDamage");
+        if (playerHealth <= 0) Die();
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        animator.SetBool("isDead", true);
+        movement = Vector3.zero;
+        
+    }
+
+    //we can see the detection radius in the editor for debugging purposes, this will help us to adjust the radius for better gameplay experience
+    private void OnDrawGizmos()
+    {
+   
+        Gizmos.color = new Color(0, 1, 0, 0.3f);//green
+        Vector3 detectionCenter = transform.position + Vector3.up * 0.5f;
+        Gizmos.DrawSphere(detectionCenter, 2.0f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(detectionCenter, 2.0f);
+    }
+}
