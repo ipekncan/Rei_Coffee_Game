4c6a86d8beee17192bda7b1af43a4cd2ef2edd08
İpek Nezihe Can <ipekn.can@gmail.com>
2026-05-07 01:17:08 +0300

    enemy movements prep

---
+++ Assets/C# Scripts/AIEnemyScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
